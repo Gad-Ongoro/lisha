c9edca6f4c066ac61dd273e1331bbda3cd8f91f1
Gad-Ongoro <gadongoro1@gmail.com>
2024-07-06 20:43:38 +0300

    new: Models

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from uuid import uuid4
 
 # Create your models here.
+# user
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=100)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    # Related_name to avoid clashes with built-in User model
+    groups = models.ManyToManyField('auth.Group', related_name='customuser_set', blank=True)
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='customuser_set', blank=True)
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUser, self).__init__(*args, **kwargs)
+
+    def __str__(self):
+        return self.email
+    
+# profile
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    secondary_email = models.EmailField(null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    county = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=100, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+# product
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_available = models.PositiveIntegerField()
+    unit_of_measurement = models.CharField(max_length=20)
+    perishable = models.BooleanField()
+    expiration_date = models.DateField()
+    image = models.ImageField(upload_to='product_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+# order 
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.ForeignKey(CustomUser, related_name='orders', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='orders', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    order_status = models.CharField(max_length=20)
+    
+# review
+class Review(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(CustomUser, related_name='reviews_given', on_delete=models.CASCADE)
+    reviewee = models.ForeignKey(CustomUser, related_name='reviews_received', on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
