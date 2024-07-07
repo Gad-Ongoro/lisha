d81408b5f1ab932663c42bcdc730ab479dc66e52
Gad-Ongoro <gadongoro1@gmail.com>
2024-07-07 23:36:28 +0300

    Added axios interceptor

---
+++ client/src/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const apiUrl = "http://127.0.0.1:8000/api/";
+
+const api = axios.create({
+    baseURL: apiUrl,
+})
+
+api.interceptors.request.use(
+    (config) => {
+		const token = localStorage.getItem('access');
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+    },
+    (error) => {
+      	return Promise.reject(error);
+    }
+  	);
+
+export default api;
