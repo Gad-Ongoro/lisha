aebf9eae94afda1c8b260a910860c18e5e76e892
Gad-Ongoro <gadongoro1@gmail.com>
2024-07-18 15:35:32 +0300

    mv ClientDash > DashHome

---
+++ client/src/components/DashBoard/DashHome.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { enqueueSnackbar } from 'notistack';
+import AnimatedYPage from '../AnimatedYPage';
+import SideNav from './SideNav';
+import DashBar from './DashBar';
+import DashView from './DashView';
+import DashDrawer from './DashDrawer';
+import Settings from './Settings';
+import ProtectedRoute from '../Auth/ProtectedRoute';
+import { AppContext } from '../../App';
+import api from '../../api';
+export const DashContext = createContext();
+
+function DashHome() {
+	const { auth, setAuth, refreshToken, navigate } = useContext(AppContext);
+	const [dashDrawerOpen, setDashDrawerOpen] = useState(false);
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > 1) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+	const handleLogout = async () => {
+    if (!refreshToken) {
+      alert('No refresh token found!');
+      return;
+    }
+  
+    try {
+      const res = await api.post('logout/', { 'refresh_token': refreshToken });
+      if (res.status === 204) {
+        setAuth(false);
+        localStorage.removeItem('access');
+        localStorage.removeItem('refresh');
+        navigate('/account/signin');
+				enqueueSnackbar(`Successfully Logged Out!`, { variant: 'success' });
+      }
+    } catch (error) {
+			enqueueSnackbar('Error Logging Out!', { variant: 'error' })
+      console.error('Logout Error:', error);
+    }
+  };
+
+	return (
+		<>
+			<ProtectedRoute>
+				<AnimatedYPage>
+					<DashContext.Provider value={{ dashDrawerOpen, setDashDrawerOpen, handleLogout }}>
+						<DashDrawer></DashDrawer>
+						<div className='container h-screen grid grid-cols-12'>
+							<div className={`sticky top-0 z-20 ${isScrolled ? 'bg-emerald-100' : 'bg-white'} 
+								transition-all duration-300
+								col-start-1 col-end-13 sm:col-start-2 sm:col-end-13 sm:p-3 rounded-lg`}
+							>
+								<DashBar></DashBar>
+							</div>
+							<div className='sticky top-20 sm:top-10 rounded col-start-1 col-end-13 
+								row-start-1 row-end-2 md:fixed md:top-1/2 md:-translate-y-1/2 md:row-start-2 md:row-end-13
+								hidden md:block'
+							>
+								<SideNav></SideNav>
+							</div>
+							<div className='row-start-2 row-end-13 col-start-1 md:col-start-2 col-end-13'>
+								<Routes>
+									<Route path='/dashview' element={<DashView></DashView>}></Route>
+									<Route path='/settings' element={<Settings></Settings>}></Route>
+								</Routes>
+							</div>
+						</div>
+					</DashContext.Provider>
+				</AnimatedYPage>
+			</ProtectedRoute>
+		</>
+	)
+}
+
+export default DashHome;
