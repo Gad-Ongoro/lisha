f296d1af1d650fc4cd16cbba1226266eed5deafd
Gad-Ongoro <gadongoro1@gmail.com>
2024-07-29 22:16:13 +0300

    fix: Updated path pattern to handle proper media serving during development

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -19,3 +21,6 @@ urlpatterns = [
     # path('api/auth/', include('djoser.urls')),
     # path('api/auth/', include('djoser.urls.jwt')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
