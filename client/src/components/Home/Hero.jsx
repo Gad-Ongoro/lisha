f767024d0b35a222f31831dc44211c7039dadd76
Gad-Ongoro <gadongoro1@gmail.com>
2024-07-30 10:22:59 +0300

    fix: Updated dead links

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 import TopWaves from '../Shapers/TopWaves';
 import BottomWaves from '../Shapers/BottomWaves';
 import HeroCarousel from './HeroCarousel';
+import { AppContext } from '../../App';
 
 const Hero = () => {
+  const { auth } = useContext(AppContext);
   return (
     <section className="relative md:flex bg-emerald-500 text-white rounded">
       <TopWaves />
@@ -14,7 +17,14 @@ const Hero = () => {
       <div className="container mx-auto px-6 py-16 text-center">
         <h1 className="text-4xl font-bold mb-2">Connecting Small-Scale Farmers and Fishermen to Markets</h1>
         <p className="text-xl mb-4">Discover fresh produce and seafood directly from local suppliers.</p>
-        <button className="bg-white text-emerald-600 font-semibold py-2 px-4 rounded">Get Started</button>
+
+        {
+          !auth && (
+            <NavLink to={'/account/usertype'}>
+              <button className="bg-white text-emerald-600 font-semibold py-2 px-4 rounded">Get Started</button>
+            </NavLink>
+          )
+        }
       </div>
     </section>
   );
