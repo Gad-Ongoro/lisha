d63eb333dc4ba76565aa445ace8d8ab82b328214
Gad-Ongoro <gadongoro1@gmail.com>
2024-08-09 23:31:52 +0300

    feat: Added bottom waves styling

--- client/src/components/Home/Categories.jsx
+++ client/src/components/Home/Categories.jsx
@@ -1,13 +1,14 @@
-// components/Categories.js
 import React from 'react';
 import CategoriesCarousel from './CategoriesCarousel';
 import TopWaves from '../Shapers/TopWaves';
+import BottomWaves from '../Shapers/BottomWaves';
 import { px } from 'framer-motion';
 
 const Categories = () => {
   return (
     <section className="relative bg-emerald-500 py-10">
       <TopWaves />
+      <BottomWaves />
       <div className="container mx-auto px-6 text-center">
         <h2 className="text-3xl font-bold mb-6">Explore Categories</h2>
         <CategoriesCarousel />
