f8175a3f9a048ba2b141a21e80c4c5b83c1c3a80
Gad-Ongoro <gadongoro1@gmail.com>
2024-08-09 23:33:36 +0300

    Updated App to fetch and pass products through the context api

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import SignIn from './components/Auth/SignIn';
 import PasswordResetRequest from './components/Auth/PswdRstReq';
 import PasswordResetConfirm from './components/Auth/PswdRstCnfrm';
 import ProductsPage from './components/Products/ProductsPage';
+import ProductPage from './components/Products/ProductPage';
 import Cart from './components/Cart/Cart';
 import api from './api';
 export const AppContext = createContext();
@@ -18,9 +19,12 @@ export const AppContext = createContext();
 function App() {
   const navigate = useNavigate();
   const [ snackBarOpen, setSnackBarOpen ] = useState(false);
+  const [ mobileMenuOpen, setMobileMenuOpen ] = useState(false);
   const [ productCategory, setProductCategory ] = useState('');
   const [ cartOpen, setCartOpen ] = useState(false);
+  const [ products, setProducts ] = useState([]);
   const [ productQuickViewOpen, setProductQuickViewOpen ] = useState(false);
+  const currency = localStorage.getItem('currency') ? localStorage.getItem('currency') : 'KES';
   const accessToken = localStorage.getItem('access');
   const refreshToken = localStorage.getItem('refresh');
   const user_id = accessToken !== null || undefined ? jwtDecode(accessToken).user_id : null;
@@ -43,6 +47,19 @@ function App() {
 		}
 	};
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await api.get('products/');
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   useEffect( () => {
     if (accessToken) {
       setAuth(true);
@@ -54,7 +71,13 @@ function App() {
 
   return (
     <div>
-      <AppContext.Provider value={{ navigate, accessToken, refreshToken, auth, setAuth, user_id, user, snackBarOpen, setSnackBarOpen, scrollToTop, cartOpen, setCartOpen, productCategory, setProductCategory, productQuickViewOpen, setProductQuickViewOpen }}>
+      <AppContext.Provider value={{ navigate, accessToken, refreshToken,
+          auth, setAuth, user_id, user, snackBarOpen, setSnackBarOpen, scrollToTop, 
+          cartOpen, setCartOpen, productCategory, setProductCategory,
+          productQuickViewOpen, setProductQuickViewOpen, currency, mobileMenuOpen, setMobileMenuOpen,
+          products, setProducts
+         }}
+      >
         {
           (auth && cartOpen && user.role === 'buyer') && <Cart />
         }
@@ -68,6 +91,7 @@ function App() {
           <Route path="/password/reset/confirm/:uid/:token" element={<PasswordResetConfirm />} />
           <Route path="/account/:user/*" element={<DashHome />} />
           <Route path="/products/:category" element={<ProductsPage />} />
+          <Route path="/products/:category/:product_id" element={<ProductPage />} />
         </Routes>
       </AppContext.Provider>
     </div>
