9c4e2f6eaf69c215f56234e2f958a6b7dec45fa7
Gad-Ongoro <gadongoro1@gmail.com>
2024-08-12 23:14:02 +0300

    new service: Currency Converter

---
+++ client/src/services/CurrencyConverter.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const currencyConverter = async () => {
+    const currency = localStorage.getItem('currency');
+    const URL = `https://v6.exchangerate-api.com/v6/${process.env.REACT_APP_CURRENCY_EXCHANGE_RATE_API_KEY}/latest/${currency}`;
+
+    const { data } = await axios.get(URL);
+    return data;
+}
