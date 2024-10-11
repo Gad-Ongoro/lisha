e4b053adac818cf437609794ffdea79f3f0393fe
Gad-Ongoro <gadongoro1@gmail.com>
2024-10-11 17:33:10 +0300

    feat: AfricasTalking SMS service

--- server/api/utils.py
+++ server/api/utils.py
@@ -1,4 +1,6 @@
 from django.core.mail import EmailMessage
+import africastalking
+from django.http import JsonResponse
 from django.conf import settings
 
 def send_normal_email(data):
@@ -9,3 +11,21 @@ def send_normal_email(data):
         to=[data['to_email']]
     )
     email.send()
+
+# SMS Service
+africastalking.initialize(
+    settings.AFRICAS_TALKING_USERNAME,
+    settings.AFRICAS_TALKING_API_KEY
+)
+
+sms = africastalking.SMS
+
+def send_sms(phone_number, details):
+    message = details
+
+    try:
+        response = sms.send(message, [phone_number])
+        return JsonResponse({"success": True, "message": "SMS sent", "response": response}, status=200)
+
+    except Exception as e:
+        return JsonResponse({"success": False, "error": f"An unexpected error occurred: {str(e)}"}, status=500)
